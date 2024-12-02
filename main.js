50f0ab85743e9ee2d3429ab6c81bd54f4475169e
kdr250 <kdr250@users.noreply.github.com>
2024-12-02 05:01:24 +0000

    deploy: aa33cce203727935e76295e43daf22e28b5799b1

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmpxdzk5yab.js
+// include: /tmp/tmpa4lmsk7m.js
 
   Module['expectedDataFileDownloads'] ??= 0;
   Module['expectedDataFileDownloads']++;
@@ -217,21 +217,21 @@ Module['FS_createPath']("/resources", "resources", true, true);
 
   })();
 
-// end include: /tmp/tmpxdzk5yab.js
-// include: /tmp/tmpqs9gvze3.js
+// end include: /tmp/tmpa4lmsk7m.js
+// include: /tmp/tmpgfop_jay.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if (Module['$ww'] || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmpqs9gvze3.js
-// include: /tmp/tmp227datg_.js
+  // end include: /tmp/tmpgfop_jay.js
+// include: /tmp/tmpp6v5efgk.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmp227datg_.js
+  // end include: /tmp/tmpp6v5efgk.js
 
 
 // Sometimes an existing Module object exists with properties
