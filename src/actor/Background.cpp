5c9b6d00ff133a652992b507d38ca04fb6c31e64
kdr250 <54400975+kdr250@users.noreply.github.com>
2024-12-12 23:34:32 +0900

    背景用のシェーダー作成

--- src/actor/Background.cpp
+++ src/actor/Background.cpp
@@ -7,7 +7,9 @@ void Background::Spawn()
     auto& assetManager  = game.GetAssetManager();
     auto& entityManager = game.GetEntityManger();
 
-    if (!assetManager.LoadShader(SPRITE_SHADER_NAME, SPRITE_SHADER_VERT, SPRITE_SHADER_FRAG))
+    if (!assetManager.LoadShader(BACKGROUND_SHADER_NAME,
+                                 BACKGROUND_SHADER_VERT,
+                                 BACKGROUND_SHADER_FRAG))
     {
         SDL_Log("Failed to load shader");
         exit(EXIT_FAILURE);
@@ -15,26 +17,23 @@ void Background::Spawn()
 
     assetManager.CreateSpriteVertex();
 
-    assetManager.LoadTexture(BACKGROUND_NAME_ONE, BACKGROUND_PATH_ONE);
+    if (!assetManager.LoadTexture(BACKGROUND_NAME_ONE, BACKGROUND_PATH_ONE))
+    {
+        SDL_Log("Failed to load texture");
+        exit(EXIT_FAILURE);
+    }
 }
 
 void Background::Draw()
 {
-    auto& game         = Game::GetGame();
-    auto& assetManager = game.GetAssetManager();
-    auto& texture      = assetManager.GetTexture(BACKGROUND_NAME_ONE);
-    auto& spriteShader = assetManager.GetShader(SPRITE_SHADER_NAME);
-    auto& vertexArray  = assetManager.GetSpriteVertex();
+    auto& game             = Game::GetGame();
+    auto& assetManager     = game.GetAssetManager();
+    auto& texture          = assetManager.GetTexture(BACKGROUND_NAME_ONE);
+    auto& backgroundShader = assetManager.GetShader(BACKGROUND_SHADER_NAME);
+    auto& vertexArray      = assetManager.GetSpriteVertex();
 
-    spriteShader.SetActive();
+    backgroundShader.SetActive();
     vertexArray.SetActive();
-
-    spriteShader.SetVector2Uniform("uWindowSize", Game::WINDOW_WIDTH, Game::WINDOW_HEIGHT);
-    spriteShader.SetVector2Uniform("uTextureSize", texture.GetWidth(), texture.GetHeight());
-    spriteShader.SetVector2Uniform("uTexturePosition",
-                                   Game::WINDOW_WIDTH / 2.0f,
-                                   Game::WINDOW_HEIGHT / 2.0f);
-    spriteShader.SetFloatUniform("uTextureScale", 2.0f);
     texture.SetActive();
 
     glDrawElements(GL_TRIANGLES, vertexArray.GetNumIndices(), GL_UNSIGNED_INT, nullptr);
