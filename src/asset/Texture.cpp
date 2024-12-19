4bda73c62d1f3a7855672b1a795f76f058ce2028
kdr250 <54400975+kdr250@users.noreply.github.com>
2024-12-19 14:27:02 +0900

    2の累乗でなくても表示できるよう修正

--- src/asset/Texture.cpp
+++ src/asset/Texture.cpp
@@ -14,6 +14,9 @@ Texture::Texture(int format, int width, int height, const void *pixels) :
     // Enable bilinear filtering
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 }
 
 void Texture::SetActive()
